4088a024db2e0e07e673f694310886a53df10502
2017000096-MarcoPineda <m.pineda.5bdb@gmail.com>
2026-04-17 13:08:09 -0600

    Add files via upload

--- CalculadoraBasica23Pineda.java
+++ CalculadoraBasica23Pineda.java
@@ -1,13 +1,17 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-    <modelVersion>4.0.0</modelVersion>
-    <groupId>com.mycompany</groupId>
-    <artifactId>CalculadoraBasica-23-Pineda</artifactId>
-    <version>1.0-SNAPSHOT</version>
-    <packaging>jar</packaging>
-    <properties>
-        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
-        <maven.compiler.release>23</maven.compiler.release>
-        <exec.mainClass>com.mycompany.calculadorabasica.pineda.CalculadoraBasica23Pineda</exec.mainClass>
-    </properties>
-</project>
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadorabasica.pineda;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class CalculadoraBasica23Pineda {
+
+    public static void main(String[] args) {
+        jfrmCalc holapos = new jfrmCalc();
+        holapos.setVisible(true);
+    }
+}
